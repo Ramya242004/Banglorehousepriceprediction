0443b3079ae77083abdd820f353236ca2b2eeffc
ramya <ramyas242004@gmail.com>
2025-04-24 01:03:06 +0530

    HTML CSS and JS uptation

--- client/app.js
+++ client/app.js
@@ -1,48 +1,122 @@
 
-// Show the login modal
-function showLogin() {
-  document.getElementById("loginModal").style.display = "block";
-}
+// // // Show the login modal
+// // function showLogin() {
+// //   document.getElementById("loginModal").style.display = "block";
+// // }
 
-// Show the signup modal
-function showSignup() {
-  document.getElementById("signupModal").style.display = "block";
-}
+// // // Show the signup modal
+// // function showSignup() {
+// //   document.getElementById("signupModal").style.display = "block";
+// // }
 
-// Show the contact modal
-function showContact() {
-  document.getElementById('contactModal').style.display = 'block';
-}
+// // // Show the contact modal
+// // function showContact() {
+// //   document.getElementById('contactModal').style.display = 'block';
+// // }
 
-// Close the modal (for any modal)
-function closeModal(modalId) {
-  document.getElementById(modalId).style.display = "none";
-}
+// // // Close the modal (for any modal)
+// // function closeModal(modalId) {
+// //   document.getElementById(modalId).style.display = "none";
+// // }
 
-// Close the modal when clicking outside of the modal content
-function handleOutsideClick(event, modalClass) {
-  const modal = document.querySelector(`.${modalClass}`);
-  if (!modal) return; // Exit if the modal does not exist
-  const modalContent = modal.querySelector(".modal-content");
-  if (!modalContent.contains(event.target)) {
-    closeModal(modalClass); // Close the modal
-  }
-}
+// // // Close the modal when clicking outside of the modal content
+// // function handleOutsideClick(event, modalClass) {
+// //   const modal = document.querySelector(`.${modalClass}`);
+// //   if (!modal) return; // Exit if the modal does not exist
+// //   const modalContent = modal.querySelector(".modal-content");
+// //   if (!modalContent.contains(event.target)) {
+// //     closeModal(modalClass); // Close the modal
+// //   }
+// // }
 
-function handleOutsideClick(event, modalId) {
-  const modalContent = document.querySelector(`#${modalId} .modal-content`);
-  if (!modalContent.contains(event.target)) {
-      closeModal(modalId);
-      window.location.href = "app.html"; // Redirect to homepage
-  }
-}
+// // function handleOutsideClick(event, modalId) {
+// //   const modalContent = document.querySelector(`#${modalId} .modal-content`);
+// //   if (!modalContent.contains(event.target)) {
+// //       closeModal(modalId);
+// //       window.location.href = "app.html"; // Redirect to homepage
+// //   }
+// // }
 
-//predicting
+// //predicting
+// function getBathValue() {
+//   var uiBathrooms = document.getElementsByName("uiBathrooms");
+//   for(var i in uiBathrooms) {
+//     if(uiBathrooms[i].checked) {
+//         return parseInt(i)+1;
+//     }
+//   }
+//   return -1; // Invalid Value
+// }
+
+// function getBHKValue() {
+//   var uiBHK = document.getElementsByName("uiBHK");
+//   for(var i in uiBHK) {
+//     if(uiBHK[i].checked) {
+//         return parseInt(i)+1;
+//     }
+//   }
+//   return -1; // Invalid Value
+// }
+
+// function onClickedEstimatePrice() {
+//   console.log("Estimate price button clicked");
+//   var sqft = document.getElementById("uiSqft");
+//   var bhk = getBHKValue();
+//   var bathrooms = getBathValue();
+//   var location = document.getElementById("uiLocations");
+//   var estPrice = document.getElementById("uiEstimatedPrice");
+
+//   var url = "http://127.0.0.1:5000/predict_home_price"; //Use this if you are NOT using nginx which is first 7 tutorials
+//   //var url = "/api/predict_home_price"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
+
+//   $.post(url, {
+//       total_sqft: parseFloat(sqft.value),
+//       bhk: bhk,
+//       bath: bathrooms,
+//       location: location.value
+//   },function(data, status) {
+//       console.log(data.estimated_price);
+//       estPrice.innerHTML = "<h2>" + data.estimated_price.toString() + " Lakh</h2>";
+//       console.log(status);
+//   });
+// }
+
+// function onPageLoad() {
+//   console.log( "document loaded" );
+//   var url = "http://127.0.0.1:5000/get_location_names"; // Use this if you are NOT using nginx which is first 7 tutorials
+//  // var url = "/api/get_location_names"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
+//   $.get(url,function(data, status) {
+//       console.log("got response for get_location_names request");
+//       if(data) {
+//           var locations = data.locations;
+//           var uiLocations = document.getElementById("uiLocations");
+//           $('#uiLocations').empty();
+//           for(var i in locations) {
+//               var opt = new Option(locations[i]);
+//               $('#uiLocations').append(opt);
+//           }
+//       }
+//   });
+// }
+
+// function redirectToCalculator() {
+//   window.location.href = "calculator.html";  // This will redirect to the calculator page.
+// }
+
+
+
+// window.onload =onPageLoad;
+
+function toggleContactPopup() {
+  const popup = document.getElementById('contactPopup');
+  popup.style.display = (popup.style.display === "none" || popup.style.display === "") ? "block" : "none";
+}
+// Predicting
 function getBathValue() {
   var uiBathrooms = document.getElementsByName("uiBathrooms");
   for(var i in uiBathrooms) {
     if(uiBathrooms[i].checked) {
-        return parseInt(i)+1;
+        return parseInt(i) + 1;
     }
   }
   return -1; // Invalid Value
@@ -52,29 +126,32 @@ function getBHKValue() {
   var uiBHK = document.getElementsByName("uiBHK");
   for(var i in uiBHK) {
     if(uiBHK[i].checked) {
-        return parseInt(i)+1;
+        return parseInt(i) + 1;
     }
   }
   return -1; // Invalid Value
 }
 
 function onClickedEstimatePrice() {
+  // Prevent default behavior
+  // event.preventDefault();
+
   console.log("Estimate price button clicked");
+
   var sqft = document.getElementById("uiSqft");
   var bhk = getBHKValue();
   var bathrooms = getBathValue();
   var location = document.getElementById("uiLocations");
   var estPrice = document.getElementById("uiEstimatedPrice");
 
-  var url = "http://127.0.0.1:5000/predict_home_price"; //Use this if you are NOT using nginx which is first 7 tutorials
-  //var url = "/api/predict_home_price"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
+  var url = "http://127.0.0.1:5000/predict_home_price"; // Use this if you are NOT using nginx
 
   $.post(url, {
       total_sqft: parseFloat(sqft.value),
       bhk: bhk,
       bath: bathrooms,
       location: location.value
-  },function(data, status) {
+  }, function(data, status) {
       console.log(data.estimated_price);
       estPrice.innerHTML = "<h2>" + data.estimated_price.toString() + " Lakh</h2>";
       console.log(status);
@@ -82,10 +159,11 @@ function onClickedEstimatePrice() {
 }
 
 function onPageLoad() {
-  console.log( "document loaded" );
-  var url = "http://127.0.0.1:5000/get_location_names"; // Use this if you are NOT using nginx which is first 7 tutorials
- // var url = "/api/get_location_names"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
-  $.get(url,function(data, status) {
+  console.log("document loaded");
+
+  var url = "http://127.0.0.1:5000/get_location_names"; // Use this if you are NOT using nginx
+
+  $.get(url, function(data, status) {
       console.log("got response for get_location_names request");
       if(data) {
           var locations = data.locations;
@@ -99,4 +177,8 @@ function onPageLoad() {
   });
 }
 
-window.onload =onPageLoad;
+function redirectToCalculator() {
+  window.location.href = "calculator.html";  // This will redirect to the calculator page.
+}
+
+window.onload = onPageLoad;
